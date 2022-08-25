33b82cb966f14cc0d063ce8827693e7ea30ab10a
sorrowwolf <414471841@qq.com>
2022-08-25 12:15:25 +0800

    test

--- src/hooks/useCopy/index.ts
+++ src/hooks/useCopy/index.ts
@@ -1,12 +1,12 @@
-import { ElMessage } from 'element-plus';
+import { ElMessage } from 'element-plus'
 
 // 复制文本
 export const useCopy = (text: string) => {
-  const input = document.createElement('input');
-  input.value = text;
-  document.body.appendChild(input);
-  input.select();
-  document.execCommand('Copy');
-  document.body.removeChild(input);
-  ElMessage.success('复制成功');
+  const input = document.createElement('input')
+  input.value = text
+  document.body.appendChild(input)
+  input.select()
+  document.execCommand('Copy')
+  document.body.removeChild(input)
+  ElMessage.success('复制成功')
 }
