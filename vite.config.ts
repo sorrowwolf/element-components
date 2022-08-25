128fc17c9fade5af8e164260272d0a95b2d8d62a
sorrowwolf <414471841@qq.com>
2022-08-25 12:19:59 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,4 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()]
-})
+export default defineConfig({ plugins: [vue(), vueJsx()] })
